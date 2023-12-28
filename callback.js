fd141cf6b1670f624c004a333aa33b30eecff2d9
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-28 17:35:25 +0530

    example-4

--- callback.js
+++ callback.js
@@ -3,6 +3,7 @@ function greet(name) {
 console.log('hi' + ' ' + name)
 }
 greet('peter');
+//example-2"
 
 function greet1(name,callback) {
     console.log('hi'+' '+name)
@@ -12,3 +13,24 @@ function callMe() {
 console.log('i am callback func');
 }
 greet1("vinay",callMe);
+
+// example-3
+function display(val) {
+    console.log(val);
+}
+function add(a,b,callback) {
+    let res =  a+b;
+   callback(res);
+}
+add(2,3,display);
+
+// example-4
+function greet2() {
+    console.log('hello world');
+}
+function sayName(name){
+    console.log('hello'+ ' ' + name);
+}
+setTimeout(greet2, 2000);
+    sayName("viraj");
+
