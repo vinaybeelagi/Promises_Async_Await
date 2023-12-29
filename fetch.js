4d3836051781ec7f2fc7a36bc17b5f3d1b415a6b
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-29 12:37:58 +0530

    example to fetch image

--- fetch.js
+++ fetch.js
@@ -1,1 +1,11 @@
-fetch("rainbow.jpg")
+// "example to fetch image" 
+console.log('about to fetch rainbow');
+fetch('rainbow.jpg')
+.then(response => {
+    console.log(response);
+    return response.blob();
+})
+.then(blob => {
+    console.log(blob);
+    document.getElementById('rainbow').src = URL.createObjectURL(blob)
+});
