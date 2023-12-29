c612e935d80d687e5f1df71c0dde7708d2e327d1
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-29 11:43:39 +0530

    argument of .then is a function that runs when the promise is rejected and receives the error

--- Promise.js
+++ Promise.js
@@ -3,3 +3,11 @@ const data = axios.get ("https://api.api-ninjas.com/v1/cats");
 data.then((res)=>{
     console.log(res.data)
 })
+//  argument of .then is a function that runs when the promise is rejected and receives the error.
+let promise = new Promise(function(resolve,reject) {
+    setTimeout(()=> reject(new Error("whoops")),1000) 
+});
+promise.then(
+    (result) => console.log(result),
+    error => console.log(error)
+)
