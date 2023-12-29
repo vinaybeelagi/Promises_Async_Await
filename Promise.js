2bb1d01c67ef9f6651a34e4aa3ef5cf0c366eae0
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-29 12:42:23 +0530

    task-Re-resolve a promise?

--- Promise.js
+++ Promise.js
@@ -11,3 +11,13 @@ promise.then(
     (result) => console.log(result),
     error => console.log(error)
 )
+
+// task-Re-resolve a promise?
+let promise = new Promise(function(resolve, reject) {
+    resolve(1);
+  
+    setTimeout(() => resolve(2), 1000);
+  });
+  
+  promise.then(alert);
+  //The output is: 1.The second call to resolve is ignored, because only the first call of reject/resolve is taken into account. Further calls are ignored.
