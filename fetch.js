ac5a1cadb69ea88db24fa6656b7327e874d3a464
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-29 15:16:40 +0530

    promise.all example-1

--- fetch.js
+++ fetch.js
@@ -1,11 +1,26 @@
 // "example to fetch image" 
-console.log('about to fetch rainbow');
-fetch('rainbow.jpg')
-.then(response => {
-    console.log(response);
-    return response.blob();
-})
-.then(blob => {
-    console.log(blob);
-    document.getElementById('rainbow').src = URL.createObjectURL(blob)
-});
+// console.log('about to fetch rainbow');
+// fetch('rainbow.jpg')
+// .then(response => {
+//     console.log(response);
+//     return response.blob();
+// })
+// .then(blob => {
+//     console.log(blob);
+//     document.getElementById('rainbow').src = URL.createObjectURL(blob)
+// });
+
+// promise.all
+let urls = [
+    'https://api.github.com/users/iliakan',
+    'https://api.github.com/users/remy',
+    'https://api.github.com/users/jeresig'
+  ];
+  let requests = urls.map(url => fetch(url));
+  console.log(requests)
+  Promise.all(requests)
+  .then(reponses =>reponses.forEach(
+    reponse => console.log(`${reponse.url} : ${reponse.status}`)
+  ));
+
+  
