c35123d5681131af0715129236de24544d6c514f
vinaybeelagi <vinaykumar.b@neoito.com>
2024-01-03 13:17:52 +0530

    task3

--- Async_Await.js
+++ Async_Await.js
@@ -106,3 +106,17 @@ class HttpError extends Error {
   }
   
   demoGithubUser();
+
+//   task-Call async from non-async
+async function wait() {
+    await new Promise(resolve => setTimeout(resolve, 1000));
+  
+    return 10;
+  }
+  
+  function f() {
+    // shows 10 after 1 second
+    wait().then(result => alert(result));
+  }
+  
+  f();
