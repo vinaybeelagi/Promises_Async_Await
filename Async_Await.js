30e11e7199355d96b78ff4ea5cf933cdce042c1c
vinaybeelagi <vinaykumar.b@neoito.com>
2024-01-03 12:37:47 +0530

    task-Rewrite using async/await

--- Async_Await.js
+++ Async_Await.js
@@ -49,4 +49,14 @@ async function display() {
     console.log(data)
 }
 display();
- 
+
+//task- Rewrite using async/await
+async function loadJson(url) {
+    let response = await fetch(url);
+    if(response.status == 200) {
+        let json = await response.json();
+    }
+    throw new Error(response.status);
+}
+ loadJson('https://javascript.info/no-such-user.json')
+ .catch(err=>console.log(err));
