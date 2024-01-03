c72557b796acfc487a55fc984c0c0735838097a0
vinaybeelagi <vinaykumar.b@neoito.com>
2024-01-03 10:02:48 +0530

    example with promise resolved

--- Async_Await.js
+++ Async_Await.js
@@ -1,5 +1,16 @@
 // async syntax
+// async function f() {
+//     return 1
+// }
+// f().then(alert)
+
+// example with promise resolved
 async function f() {
-    return 1
+    let promise = new Promise((resolve,reject)=>{
+        setTimeout(()=>resolve("done!"),1000)
+    });
+
+    let result = await promise;
+    console.log(result);
 }
-f().then(alert)
+f();
